ca5f512e9e60656def3dc17d73a67481cde0c656
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-31 13:09:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods, Findable
-  include Paramable, Memorable::InstanceMethods
+  include Memorable::InstanceMethods, Paramable
   attr_accessor :name
   attr_reader :songs
 
